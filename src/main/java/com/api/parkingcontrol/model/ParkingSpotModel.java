9c8ebe6f4e77bb4353034fe6ef3ed823458286bc
Wendell Bruno <wendell_101@hotmail.com>
2022-04-26 17:51:29 -0300

    .

---
+++ src/main/java/com/api/parkingcontrol/model/ParkingSpotModel.java
@@ -0,0 +1,48 @@
+package com.api.parkingcontrol.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Setter
+@Getter
+@Entity
+@Table(name="tb_parking_spot")
+public class ParkingSpotModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true, length = 10)
+    private String parkingSpotNumber;
+
+    @Column(nullable = false, length = 70)
+    private String licensePlateCar;
+
+    @Column(nullable = false, length = 70)
+    private String brandCar;
+
+    @Column(nullable = false, length = 70)
+    private String modelCar;
+
+    @Column(nullable = false, length = 70)
+    private String colorCar;
+
+    @Column(nullable = false)
+    private LocalDateTime registrationDate;
+
+    @Column(nullable = false, length = 130)
+    private String responsibleName;
+
+    @Column(nullable = false, length = 30)
+    private String apartment;
+
+    @Column(nullable = false, length = 30)
+    private String block;
+}
